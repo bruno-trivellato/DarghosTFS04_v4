e8a320ef6ee387b31707ca3b228ecddd34b7f1e9
Leandro <leandro_perrotta@hotmail.com>
2013-03-21 13:46:26 -0300

    * mudança preço pvp bless de 190k para 120k

--- npc/scripts/game/bless/pvpbless.lua
+++ npc/scripts/game/bless/pvpbless.lua
@@ -7,7 +7,7 @@ function onCreatureDisappear(cid) 			npcHandler:onCreatureDisappear(cid) 		end
 function onCreatureSay(cid, type, msg) 		npcHandler:onCreatureSay(cid, type, msg) 	end
 function onThink() 							npcHandler:onThink() 						end
 
-keywordHandler:addKeyword({'bless', 'benção', 'bencao', 'twist of fate', 'pvp bless'}, D_CustomNpcModules.offerBlessing, {npcHandler = npcHandler, onlyFocus = true, ispvp = true, cost = 190000})
+keywordHandler:addKeyword({'bless', 'benção', 'bencao', 'twist of fate', 'pvp bless'}, D_CustomNpcModules.offerBlessing, {npcHandler = npcHandler, onlyFocus = true, ispvp = true, cost = 120000})
 keywordHandler:addKeyword({'job', 'trabalho', 'ajudar'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Eu ajudo os novatos que passam por aqui. Eu também sou autorizado a abençoar com a {twist of fate}, a benção para o {pvp}.'})
 
 
