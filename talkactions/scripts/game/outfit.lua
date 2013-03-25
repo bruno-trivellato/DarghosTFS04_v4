f800d9ea83de69561b83cfda3f94f48f5d1dbca3
Leonardo <leo_loko.17@hotmail.com>
2013-03-25 04:59:13 -0300

    Acrescentei 1 hunt de frost, Quest Mermaid comb, alguns bugs no mapa arrumados e ariadna quest deletada.

--- talkactions/scripts/game/outfit.lua
+++ talkactions/scripts/game/outfit.lua
@@ -1,48 +1,48 @@
-local config = {
-exhaustionInSeconds = 30,
-storage = 34534
+  local config = {
+        exhaustionInSeconds = 30,
+        storage = 34534
 }
 
 function onSay(cid, words, param)
-if(exhaustion.check(cid, config.storage) == TRUE) then
-doPlayerSendCancel(cid, "Voçê só pode alterar o Outfit da guild a cada " .. config.exhaustionInSeconds .. " segundos.")
-return TRUE
-end
+        if(exhaustion.check(cid, config.storage) == TRUE) then
+                doPlayerSendCancel(cid, "You can change outfit only 1 time per " .. config.exhaustionInSeconds .. " seconds.")
+                return TRUE
+        end
 
-local playerGuild = getPlayerGuildId(cid)
-if(playerGuild == FALSE) then
-doPlayerSendCancel(cid, "Desculpe, mais voçê não tem guild.")
-return TRUE
-end
+        local playerGuild = getPlayerGuildId(cid)
+        if(playerGuild == FALSE) then
+                doPlayerSendCancel(cid, "Sorry, you're not in a guild.")
+                return TRUE
+        end
 
-local playerGuildLevel = getPlayerGuildLevel(cid)
-if(playerGuildLevel < GUILDLEVEL_LEADER) then
-doPlayerSendCancel(cid, "Voçê tem que ser Lider de uma guild para executar este comando!")
-return TRUE
-end
+        local playerGuildLevel = getPlayerGuildLevel(cid)
+        if(playerGuildLevel < GUILDLEVEL_LEADER) then
+                doPlayerSendCancel(cid, "You have to be Leader of your guild to change outfits!")
+                return TRUE
+        end
 
-local players = getPlayersOnline()
-local outfit = getCreatureOutfit(cid)
-local message = "*Guild* Seu Outfit foi mudado pelo lider da guild. (" .. getCreatureName(cid) .. ")"
-local members = 0
-local tmp = {}
-for i, tid in ipairs(players) do
-if(getPlayerGuildId(tid) == playerGuild and cid ~= tid) then
-tmp = outfit
-if(canPlayerWearOutfit(tid, outfit.lookType, outfit.lookAddons) ~= TRUE) then
-local tidOutfit = getCreatureOutfit(tid)
-tmp.lookType = tidOutfit.lookType
-tmp.lookAddons = tidOutfit.lookAddons
-end
+        local players = getPlayersOnline()
+        local outfit = getCreatureOutfit(cid)
+        local message = "*Guild* Your outfit has been changed by leader. (" .. getCreatureName(cid) .. ")"
+        local members = 0
+        local tmp = {}
+        for i, tid in ipairs(players) do
+                if(getPlayerGuildId(tid) == playerGuild and cid ~= tid) then
+                        tmp = outfit
+                        if(canPlayerWearOutfit(tid, outfit.lookType, outfit.lookAddons) ~= TRUE) then
+                                local tidOutfit = getCreatureOutfit(tid)
+                                tmp.lookType = tidOutfit.lookType
+                                tmp.lookAddons = tidOutfit.lookAddons
+                        end
 
-doSendMagicEffect(getCreaturePosition(tid), 66)
-doCreatureChangeOutfit(tid, tmp)
-doPlayerSendTextMessage(tid, MESSAGE_INFO_DESCR, message)
-members = members + 1
-end
-end
+                        doSendMagicEffect(getCreaturePosition(tid), 66)
+                        doCreatureChangeOutfit(tid, tmp)
+                        doPlayerSendTextMessage(tid, MESSAGE_INFO_DESCR, message)
+                        members = members + 1
+                end
+        end
 
-exhaustion.set(cid, config.storage, config.exhaustionInSeconds)
-doPlayerSendCancel(cid, "Outfit da guild foi mudado com sucesso. (Total de Mudanças: " .. members .. ")")
-return TRUE
+        exhaustion.set(cid, config.storage, config.exhaustionInSeconds)
+        doPlayerSendCancel(cid, "Guild members outfit has been changed. (Total: " .. members .. ")")
+        return TRUE
 end
