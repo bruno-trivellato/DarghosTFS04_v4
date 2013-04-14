b342193d3321e9bddc7e84f9ae82c3213909085e
Leandro <leandro_perrotta@hotmail.com>
2013-04-14 17:40:28 -0300

    * fix exhasuted on potions

--- actions/scripts/liquids/potions.lua
+++ actions/scripts/liquids/potions.lua
@@ -30,8 +30,7 @@ local POTIONS = {
 }
 
 local exhaust = createConditionObject(CONDITION_EXHAUST)
-setConditionParam(exhaust, CONDITION_PARAM_TICKS, 1000)
-setConditionParam(exhaust, CONDITION_PARAM_SUBID, EXHAUST_OTHER)
+setConditionParam(exhaust, CONDITION_PARAM_TICKS, (getConfigInfo('timeBetweenExActions') - 100))
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local potion = POTIONS[item.itemid]
