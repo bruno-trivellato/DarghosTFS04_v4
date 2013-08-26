1bd65b7477da06a8f9ffe087ca439049734501c4
Leandro <leandro_perrotta@hotmail.com>
2013-08-26 17:37:41 -0300

    * ddos protection up when no connection activity is detected

--- game.cpp
+++ game.cpp
@@ -6620,7 +6620,7 @@ void Game::emergencyDDoSLoop()
 		bool brokenPps, brokenRxBps, brokenTxBps;
 		brokenPps = brokenRxBps = brokenTxBps = false;
 		
-		if(avgPps >= g_config.getNumber(ConfigManager::DDOS_EMERGENCY_PPS_TO_ENABLE))
+		if(avgPps >= g_config.getNumber(ConfigManager::DDOS_EMERGENCY_PPS_TO_ENABLE) || avgPps == 0)
 			brokenPps = true;
 		if(avgRxBps >= g_config.getNumber(ConfigManager::DDOS_EMERGENCY_RX_BPS_TO_ENABLE))
 			brokenRxBps = true;
