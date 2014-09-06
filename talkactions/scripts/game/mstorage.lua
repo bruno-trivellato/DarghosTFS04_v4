38e04b1967eba2ae1a3152ec35eb8db3ea5379ec
leandro <leandro_perrotta@hotmail.com>
2014-09-06 15:55:20 -0300

    Added /mstorage to change all storage os players in real time.

---
+++ talkactions/scripts/game/mstorage.lua
@@ -0,0 +1,18 @@
+function onSay(cid, words, param)
+	local t = string.explode(param, ",")
+	if(not t[2]) then
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Invalid param specified.")
+		return true
+	end
+
+	db.executeQuery("UPDATE `player_storage` SET `value` = " .. t[2]  .. " WHERE `key` = " .. t[1] .. ";")
+
+	for _, pid in ipairs(getPlayersOnline()) do
+
+		setPlayerStorageValue(pid, t[2], t[3])
+	end	
+
+	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "All players storage [" .. t[1] .. "] = " .. t[2])
+
+	return true
+end
