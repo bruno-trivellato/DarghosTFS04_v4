0045b2bbfc67ce0919a96f105211d7aeb474642b
leandro <leandro_perrotta@hotmail.com>
2015-07-23 20:15:41 -0300

    * fix magic wall abuse

--- spells/scripts/support/magic_wall_rune.lua
+++ spells/scripts/support/magic_wall_rune.lua
@@ -4,5 +4,21 @@ setCombatParam(combat, COMBAT_PARAM_CREATEITEM, 1497)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
 
 function onCastSpell(cid, var)
+	
+	local COOLDOWN = 8
+	
+	if(doPlayerIsInBattleground(cid)) then
+		local v = getPlayerStorageValue(cid, sid.BATTLEGROUND_LAST_MAGIC_WALL)
+		local inCooldown = (v ~= -1 and v + COOLDOWN > os.time()) and true or false
+		if(inCooldown) then
+			local cooldownLeft = (v + COOLDOWN) - os.time()
+			doPlayerSendCancel(cid, "Você está exausto para usar novamente esta runa, aguarde mais " .. cooldownLeft .. " segundos.")
+			doSendMagicEffect(getPlayerPosition(cid), CONST_ME_POFF)
+			return false
+		end
+		
+		setPlayerStorageValue(cid, sid.BATTLEGROUND_LAST_MAGIC_WALL, os.time())
+	end
+	
 	return doCombat(cid, combat, var)
 end
