238b9df10bb52613364130a0d17972497d722e42
Leandro <leandro_perrotta@hotmail.com>
2016-08-11 18:37:28 -0300

    * wild growth cannot be used inside Ent Event

--- spells/scripts/support/wild_growth.lua
+++ spells/scripts/support/wild_growth.lua
@@ -3,5 +3,12 @@ setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_EARTH)
 setCombatParam(combat, COMBAT_PARAM_CREATEITEM, 1499)
 
 function onCastSpell(cid, var)
+
+	if getPlayerStorageValue(cid, sid.ENT_INSIDE) == 1 then
+		doPlayerSendCancel(cid, "Runa não permitida dentro do evento.")
+		doSendMagicEffect(getPlayerPosition(cid), CONST_ME_POFF)		
+		return false
+	end
+
 	return doCombat(cid, combat, var)
 end
